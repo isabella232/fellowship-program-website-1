ba31aa1a8a65cf6ac5434c41dc6baa5005b4e52c
Robert Cheetham <cheetham@azavea.com>
2017-03-05 12:48:56 -0500

    added a benefits FAQ
    also fixed a type

--- data/faq.js
+++ data/faq.js
@@ -2,13 +2,15 @@ faq = {
 
   "What are the requirements to be accepted for Azavea Fellowship?" : "fellow_requirements.md",
 
-  "I have an own project idea!" : "own_project_idea.md",
+  "I have my own project idea!" : "own_project_idea.md",
 
   "How do I submit a proposal?" : "submitting-a-proposal.md",
 
   "What is the timeline for the application" : "timeline.md",
 
   "What are my obligations as a student during the Summer 2017 Azavea Fellowship Program?" : "fellow_obligations.md",
+  
+  "Apart from the stipend, am I eligible for other benefits?" : "fellow-benefits.md",
 
   "What if I have additional questions?" : "additional-questions.md"
 }
